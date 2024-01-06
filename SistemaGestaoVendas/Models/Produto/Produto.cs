ec7a81a87d7d35b3b07d7da3506861515ec249c6
Tiago <tiagoramos049@gmail.com>
2024-01-05 22:44:55 -0300

    novas alterações

--- SistemaGestaoVendas/Models/Produto/Produto.cs
+++ SistemaGestaoVendas/Models/Produto/Produto.cs
@@ -3,11 +3,11 @@
     public class Produto
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
         public decimal Preco_Unitario { get; set; }
         public decimal Quantidade_Estoque { get; set; }
-        public string Unidade_Medida { get; set; }
-        public string Link_Foto { get; set; }
+        public string? Unidade_Medida { get; set; }
+        public string? Link_Foto { get; set; }
     }
 }
--- SistemaGestaoVendas/obj/Debug/net8.0/SistemaGestaoVendas.AssemblyInfo.cs
+++ SistemaGestaoVendas/obj/Debug/net8.0/SistemaGestaoVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b504c0d826e3b4c11d95a6b61a0414d207c43763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ecb73ca3e756966e6077d5fe87c0b89d5e87992")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
