60eff7a4a2ffd5dbb9688464909a6788e8c7a94c
Tiago <tiagoramos049@gmail.com>
2024-02-18 20:09:19 -0300

    wer

--- SistemaGestaoVendas/obj/Debug/net8.0/SistemaGestaoVendas.AssemblyInfo.cs
+++ SistemaGestaoVendas/obj/Debug/net8.0/SistemaGestaoVendas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bed0c5fee66835d397ab8de794bb8826e0e6276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28e8f12b164380e799e22c7d35ca1979268ee7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestaoVendas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
